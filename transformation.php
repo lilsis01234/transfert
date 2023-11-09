b4b034369daf51d4e071c63528869aecbe4c425d
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-11-09 16:03:31 +0300

    NEVA FOWGET DIS

--- transformation.php
+++ transformation.php
@@ -2,3 +2,13 @@ SET description = REPLACE(description, "&Eacute;", "É") i learned today
 SET description = CONCAT(description,"thing to concat")
 UPDATE sb8_product_lang
 SET name = CONCAT(UPPER(SUBSTRING_INDEX(name, ' ', 1)), ' ', LOWER(SUBSTRING(name, LENGTH(SUBSTRING_INDEX(name, ' ', 1)) + 2)))
+
+593
+<a href="https://tour.previsite.com/6C4C9B42-5DDA-5CCF-8BF5-4C5F003B454A" onclick="window.open(this.href,'','resizable=no,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,fullscreen=no,dependent=no,status'); return false">Pour une visite Virtuelle cliquez ici : </a>
+aorinany le 1010 euros 
+
+580
+<a href="https://tour.previsite.com/A5E01337-76B9-D2C0-1955-E879E7611768" onclick="window.open(this.href,'','resizable=no,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,fullscreen=no,dependent=no,status'); return false">Pour une Visite Virtuelle cliquez ici : </a>
+aorinany p class comment
+
+586
