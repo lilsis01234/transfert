e6205cce553239b4d6c8d7f1292752e5f96fce85
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-11-13 16:43:10 +0300

    new things i learned

--- transformation.php
+++ transformation.php
@@ -1,6 +1,6 @@
 SET description = REPLACE(description, "&Eacute;", "É") i learned today 
 SET description = CONCAT(description,"thing to concat")
-UPDATE sb8_product_lang
+UPDATE ps_product_lang
 SET name = CONCAT(UPPER(SUBSTRING_INDEX(name, ' ', 1)), ' ', LOWER(SUBSTRING(name, LENGTH(SUBSTRING_INDEX(name, ' ', 1)) + 2)))
 
 593
